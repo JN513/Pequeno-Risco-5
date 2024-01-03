2ab8b7ac5dc00b9eb915271a1d722d0b763ce9b9
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-01-03 11:00:04 -0300

    Realizando pequenas correções no ALU_Control
    Signed-off-by: Julio Nunes Avelar <jn5.guape@gmail.com>

--- src/alu_control.v
+++ src/alu_control.v
@@ -11,18 +11,18 @@ assign aluop_out = aluop_out_reg;
 
 always @(*) begin
     case ({aluop_in, func7, func3})
-        12'b00xxxxxxxxxx:  
-            aluop_out_reg = 0010;
-        12'b01xxxxxxxxxx:  
-            aluop_out_reg = 0110;
-        12'b1xxxxxxxx000:  
-            aluop_out_reg = 0010;
-        12'b1xx1xxxxx000:  
-            aluop_out_reg = 0110;
-        12'b1x0000000111:  
-            aluop_out_reg = 0000;
-        12'b1x0000000110:  
-            aluop_out_reg = 0001;
+        12'b00_0000000_010:  
+            aluop_out_reg = 4'b0010;
+        12'bx1_xxxxxxx_xxx:  
+            aluop_out_reg = 4'b0110;
+        12'b1x_0000000_000:  
+            aluop_out_reg = 4'b0010;
+        12'b1x_0100000_000:  
+            aluop_out_reg = 4'b0110;
+        12'b1x_0000000_111:  
+            aluop_out_reg = 4'b0000;
+        12'b1x_0000000_110:  
+            aluop_out_reg = 4'b0001;
     endcase
 end
     
