de8cbe591fa4f22a28ea346fafed78c94c6c13ad
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-01-16 17:08:00 -0300

    Criando novos exemplos em assembly
    Signed-off-by: Julio Nunes Avelar <jn5.guape@gmail.com>

--- software/generate_mem.sh
+++ software/generate_mem.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-riscv32-unknown-elf-as -march=rv32i teste1.S -o build/teste1.o
-riscv32-unknown-elf-ld build/teste1.o -o build/teste1.elf
-riscv32-unknown-elf-objcopy -O binary build/teste1.elf build/teste1.bin
-hexdump -v -e '1/4 "%08x" "\n"' build/teste1.bin > teste.hex
+riscv32-unknown-elf-as -march=rv32i addi.S -o build/addi.o
+riscv32-unknown-elf-ld build/addi.o -o build/addi.elf
+riscv32-unknown-elf-objcopy -O binary build/addi.elf build/addi.bin
+hexdump -v -e '1/4 "%08x" "\n"' build/addi.bin > addi.hex
