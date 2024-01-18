e3fb5268c9f0156ff7597f2f25391878e69d255f
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-01-17 22:00:12 -0300

    exemplos para as tang nano

--- fpga/tangnano9k/main.v
+++ fpga/tangnano9k/main.v
@@ -2,14 +2,19 @@ module top (
     input wire clk,
     input wire reset,
     input wire rx,
-    output wire tx
+    output wire tx,
+    output wire [5:0]led
 );
 
+wire [7:0]leds;
+assign led = [5:0]leds;
+
 Core #(
-    .MEMORY_FILE("../software/memory/beq.hex")
+    .MEMORY_FILE("../software/memory/fpga_test.hex")
 ) Core(
     .clk(clk),
-    .reset(reset)
+    .reset(reset),
+    .leds(leds)
 );
 
 always @(posedge clk ) begin
